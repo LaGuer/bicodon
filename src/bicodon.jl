3da1f6c2cb330a75d1f2375c8baf1c60701b5f85
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-05-04 16:17:59 -0700

    Create bicodon.jl

---
+++ src/bicodon.jl
@@ -0,0 +1,29 @@
+__precompile__()
+module BiCodon
+
+using Measurements, Unitful, Bio
+import Measurements: value, uncertainty
+import Unitful: AbstractQuantity
+import BioSequences
+import GenomicFeatures
+import BioAlignments
+import BioStructures
+import GeneticVariation
+import BioServices
+import BioTools
+
+# TODO
+#import Phylogenies
+
+const Seq = BioSequences
+const Intervals = GenomicFeatures
+const Align = BioAlignments
+const Structure = BioStructures
+const Var = GeneticVariation
+const Services = BioServices
+const Tools = BioTools
+
+# TODO
+#const Phylo = Phylogenies
+
+end  # module BiCodon
